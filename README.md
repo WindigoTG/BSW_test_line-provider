6bad9e70a7f5a2492a439ec1bc1f3276660437e5
Maxim Olesov <windigotp@gmail.com>
2024-09-06 19:27:05 +0800

    add endpoints doc notation

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# BSW_test_line-provider
+# Betting Software Test line-provider
+
+Сервис line-provider, компаньон для [bet-maker](https://github.com/WindigoTG/BSW_test_bet-maker)
--- src/api/routers/event.py
+++ src/api/routers/event.py
@@ -10,8 +10,13 @@ from src.utils.unit_of_work import UnitOfWork
 router = APIRouter()
 
 
-@router.get('/events')
+@router.get(
+    '/events',
+    response_model=EventListWrapper,
+    summary="Get list of active events."
+)
 async def get_events(uow: UnitOfWork = Depends(UnitOfWork)):
+    """Get list of active events."""
     events = await EventService.get_active_events(uow, int(time.time()))
     return EventListWrapper(
         payload=[
@@ -21,11 +26,23 @@ async def get_events(uow: UnitOfWork = Depends(UnitOfWork)):
     )
 
 
-@router.post('/event')
+@router.post(
+    '/event',
+    response_model=EventWrapper,
+    summary="Create new event.",
+)
 async def create_event(
     event: CreateEventSchema,
     uow: UnitOfWork = Depends(UnitOfWork)
 ):
+    """
+    Create new event.
+
+    Body params:
+    - **coefficient**: Bet coefficient for the event.
+    - **deadline**: Timestamp of betting dedline.
+    - **state**: State of the event.
+    """
     event = await EventService.add_one_and_get_obj(
         uow,
         coefficient=event.coefficient,
@@ -35,11 +52,24 @@ async def create_event(
     return EventWrapper(status=201, payload=EventSchema.model_validate(event))
 
 
-@router.put('/event')
-async def create_event(
+@router.put(
+    '/event',
+    response_model=EventWrapper,
+    summary="Update event data.",
+)
+async def update_event(
     event: UpdateEventSchema,
     uow: UnitOfWork = Depends(UnitOfWork)
 ):
+    """
+    Update event data
+
+    Body params:
+    - **id**: Id of the event.
+    - **coefficient**: Bet coefficient for the event.
+    - **deadline**: Timestamp of betting dedline.
+    - **state**: State of the event.
+    """
     event = await EventService.update_one_by_id(
         uow,
         event.id,
@@ -54,8 +84,17 @@ async def create_event(
     return EventWrapper(status=200, payload=EventSchema.model_validate(event))
 
 
-@router.get('/event/{event_id}')
-async def get_events(event_id: int, uow: UnitOfWork = Depends(UnitOfWork)):
+@router.get(
+    '/event/{event_id}',
+    summary="Get a single event by id."
+)
+async def get_event(event_id: int, uow: UnitOfWork = Depends(UnitOfWork)):
+    """
+    Get a single event by id.
+
+    Path params:
+    - **event_id**: Id of the event.
+    """
     event = await EventService.get_by_query_one_or_none(uow, id=event_id)
     if not event:
         return BaseWrapper(error=True, status=404)
